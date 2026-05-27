1720c6990e81d06246b43f088d30318264176387
mjun6356 <mjun6356@g.shingu.ac.kr>
2026-05-27 18:34:36 +0900

    음향 다운받음 ㅋㅋ

---
+++ finalgame/Assets/Script/MenuManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject helpPanel;
+    
+    
+
+    
+
+    // 1. '게임 시작' 버튼을 누르면 실행될 함수
+    public void StartGame()
+    {
+        
+        SceneManager.LoadScene("Would");
+
+        
+    }
+
+    // 2. '도움말' 버튼을 누르면 실행될 함수
+    public void ShowHelp()
+    {
+        // 도움말 창을 화면에 보이게 켭니다.
+        helpPanel.SetActive(true);
+    }
+
+    // 3. 도움말 창을 닫고 싶을 때 쓸 함수 
+    public void CloseHelp()
+    {
+        // 도움말 창을 다시 안 보이게 끕니다.
+        helpPanel.SetActive(false);
+    }
+}
