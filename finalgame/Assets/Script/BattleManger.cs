76ba73c231bf7f12bdcd685f181c263b716ff5bf
mjun6356 <mjun6356@g.shingu.ac.kr>
2026-06-04 15:12:41 +0900

    음음음귀찮다

--- finalgame/Assets/Script/BattleManger.cs
+++ finalgame/Assets/Script/BattleManger.cs
@@ -1,8 +1,19 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// 전투의 상태를 정의
+public enum BattleState { START,PLAYMENU, PLAYACTION, ENEMYTURN, CHECK }
+
 public class BattleManger : MonoBehaviour
 {
+    public BatteryStatus currentState;
+
+    //UI 패널들 연결
+    public GameObject MainPanel;
+    public GameObject attackTargetPanel;
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
